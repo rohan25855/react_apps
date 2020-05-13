1b36d22d0f8c4470270bc0a5e0e136250285e989
Rohan Saxena <rohan25855@gmail.com>
2020-05-13 20:05:29 +0530

    Updates

---
+++ precache-manifest.828dbddf1f4411577c292d2a7a695aec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c55b8a02786ce42758306c531fefafc",
+    "url": "/react_apps/index.html"
+  },
+  {
+    "revision": "5f24c60ad6e62cdc3c46",
+    "url": "/react_apps/static/css/main.acb2ade7.chunk.css"
+  },
+  {
+    "revision": "a740ed8f3265a1da9ecc",
+    "url": "/react_apps/static/js/2.af71590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_apps/static/js/2.af71590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f24c60ad6e62cdc3c46",
+    "url": "/react_apps/static/js/main.1c0e2e0c.chunk.js"
+  },
+  {
+    "revision": "153e6f3daeeb4e9b26d7",
+    "url": "/react_apps/static/js/runtime-main.4303b0e3.js"
+  }
+]);
